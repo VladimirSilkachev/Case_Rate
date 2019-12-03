5ac506ea0893729b33a422c253395990a5615c2c
aigerimka <aigerish0603@gmail.com>
2019-12-03 12:23:30 +0700

    1.0

--- solution.py
+++ solution.py
@@ -1,41 +1,36 @@
 """Case-study #4 Парсинг web-страниц
 Разработчики:
-Иванов А.С., Петров П.С., Сидоров C.H.
+Силкачев В.В. 35%, Попов К.М. 45%, Винников А.А. 35%
 
 """
 import urllib.request
 
-url = 'http://www.nfl.com/player/ryanmallett/2495443/profile'
-f = urllib.request.urlopen(url)
-s = f.read()
-text = str(s)
-print(text)
-part_name = text.find("player-name")
-name = text[text.find('>', part_name) + 1:text.find('&', part_name)]
-part_stat = text.find('TOTAL')
-stat = text[text.find('>', part_stat) + 1:text.find('</tr>', part_stat)].replace('t','').replace('n','')
-stat = stat.replace('\\','').replace('<d>','').split('</d>')
-print(name)
-print(stat)
-ATT = float(stat[1])
-COMP = float(stat[0])
-YDS = float((stat[3]).replace(',',''))
-TD = float(stat[5])
-INT = float(stat[6])
-a = (COMP/ATT) / 6
-b = (YDS/ATT - 3) * .25
-c = (TD/ATT) * 20
-d = (2.375 - (INT/ATT * 25))
-PR = ((a + b + c + d)/6) * 100
-print('{0:.2f}'.format(PR))
-# Файл input.txt
-
 with open('output.txt', "w") as f:
-    f.write("Name: " + name)
-    f.write("\nCOMP: " + str(COMP))
-    f.write("\nATT: " + str(ATT))
-    f.write("\nYDS: " + str(YDS))
-    f.write("\nTD: " + str(TD))
-    f.write("\nINT: " + str(INT))
-    f.write("\nPR: " + str(('{0:.2f}'.format(PR))))
+    pass
+
+with open('input.txt', 'r') as inp_file:
+    lst_file = inp_file.readlines()
+for url in lst_file:
+
+    f = urllib.request.urlopen(url)
+    s = f.read()
+    text = str(s)
+    part_name = text.find("player-name")
+    name = text[text.find('>', part_name) + 1:text.find('&', part_name)]
+    part_stat = text.find('TOTAL')
+    stat = text[text.find('>', part_stat) + 1:text.find('</tr>', part_stat)].replace('t','').replace('n','')
+    stat = stat.replace('\\','').replace('<d>','').split('</d>')
+    ATT = int((stat[1]).replace(',',''))
+    COMP = int((stat[0]).replace(',',''))
+    YDS = int((stat[3]).replace(',',''))
+    TD = int((stat[5]).replace(',',''))
+    INT = int((stat[6]).replace(',',''))
+    a = (COMP/ATT) / 6
+    b = (YDS/ATT - 3) * .25
+    c = (TD/ATT) * 20
+    d = (2.375 - (INT/ATT * 25))
+    PR = ((a + b + c + d)/6) * 100
+    PR = ('{0:.2f}'.format(PR))
+    with open('output.txt', "a") as f:
+        f.write(name + ' ' + str(COMP) + ' ' + str(ATT) + ' ' + str(YDS) + ' ' + str(TD) + ' ' + str(INT) + ' ' + PR + '\n')
 
